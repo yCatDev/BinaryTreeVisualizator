06b348cae411dbd9d49d728ee5e68bef719a18bf
Grebenuk Denis <ycat.developer@gmail.com>
2020-05-20 23:53:49 +0300

    Initial commit

---
+++ BinaryTreeVisualizator/BinaryTreeVisualizator/Game1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Xna.Framework;
+using Nez;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BinaryTreeVisualizator
+{
+    public class MyGame : Core
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
---
+++ BinaryTreeVisualizator/BinaryTreeVisualizator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BinaryTreeVisualizator
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var game = new MyGame();
+            game.Run();
+        }
+    }
+}
